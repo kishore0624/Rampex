06388d39a7d598c8c808a05e36da7a7c27ac14ca
Kishosre Kumar S <kishore061224@gmail.com>
2026-06-25 10:44:07 +0530

    Program to swapping to variables

---
+++ src/Day_1/Swaping.java
@@ -0,0 +1,10 @@
+package Day_1;
+
+public class Swaping {
+    public static void main(String[] args) {
+        int a = 5 , b = 10;
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+    }
+}
