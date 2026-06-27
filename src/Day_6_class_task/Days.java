412e4e48e9300345a6961e5a552504b3bb05ed1f
Kishosre Kumar S <kishore061224@gmail.com>
2026-06-27 06:43:25 +0530

    Days total

---
+++ src/Day_6_class_task/Days.java
@@ -0,0 +1,19 @@
+package Day6.class_Task;
+
+public class Days {
+
+    public static void main(String[] args) {
+
+        int totalDays = 0;
+
+        for (int month = 1; month <= 12; month++) {
+            for (int week = 1; week <= 4; week++) {
+                for (int day = 1; day <= 7; day++) {
+                    totalDays++;
+                }
+            }
+        }
+
+        System.out.println("Total Days = " + totalDays);
+    }
+}
