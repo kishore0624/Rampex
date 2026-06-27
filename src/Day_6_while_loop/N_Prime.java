62edc1bdd81c3f234eec01b461b6f1e1537a7ee8
Kishosre Kumar S <kishore061224@gmail.com>
2026-06-27 07:05:13 +0530

    N prime numbers

---
+++ src/Day_6_while_loop/N_Prime.java
@@ -0,0 +1,38 @@
+package Day6.class_Task;
+
+import java.util.Scanner;
+
+public class N_Prime {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+
+        int count = 0;
+        int num = 2;
+
+        while (count < n) {
+
+            boolean isPrime = true;
+
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(num + " ");
+                count++;
+            }
+
+            num++;
+        }
+
+        sc.close();
+    }
+}
