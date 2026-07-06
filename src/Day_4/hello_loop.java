8f0369907cd365ff1f36c060fbe59597f774d5e6
Kishosre Kumar S <kishore061224@gmail.com>
2026-07-06 13:47:25 +0530

    Hello loop uploaded

---
+++ src/Day_4/hello_loop.java
@@ -0,0 +1,9 @@
+package Day_4;
+
+public class hello_loop {
+    public static void main(String[] args){
+        for(int i=1;i<20;i++){
+            System.out.println("hello");
+        }
+    }
+}
