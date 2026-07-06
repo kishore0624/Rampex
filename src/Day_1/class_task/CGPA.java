c50df3e4e15f471bfd2a030292cdb26cd762ca34
Kishosre Kumar S <kishore061224@gmail.com>
2026-07-06 11:44:53 +0530

    CGPA uploaded

---
+++ src/Day_1/class_task/CGPA.java
@@ -0,0 +1,10 @@
+package Day_1.class_task;
+
+public class CGPA {
+    public static void main(String[] args)
+    {
+        int age =19;
+        double cgpa = 8.4;
+        System.out.println("My age is "+ age + " and cgpa is " + cgpa);
+    }
+}
