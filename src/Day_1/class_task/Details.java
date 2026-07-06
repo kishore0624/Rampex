f0cd1b9eb4f5e79f51f1293e2bdb1bf7d78d158f
Kishosre Kumar S <kishore061224@gmail.com>
2026-07-06 11:47:17 +0530

    Details uploaded

---
+++ src/Day_1/class_task/Details.java
@@ -0,0 +1,12 @@
+package Day_1.class_task;
+
+public class Details {
+    public static void main(String[] args)
+    {
+        char initial = 'S';
+        String name = "Kishore Kumar";
+        int age =19;
+        double cgpa = 8.4;
+        System.out.println(" My name is " + name +" ." + initial + " of age  " + age + " with the cgpa of " + cgpa);
+    }
+}
