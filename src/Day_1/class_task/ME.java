6a0069817aa4637da2ccd2505fa121275c0f5def
Kishosre Kumar S <kishore061224@gmail.com>
2026-07-06 11:52:32 +0530

    ME uploaded

---
+++ src/Day_1/class_task/ME.java
@@ -0,0 +1,8 @@
+package Day_1.class_task;
+
+public class ME {
+    public static void main(String[] args)
+    {
+        System.out.println(" This is my first program ' kishore kumar ' ");
+    }
+}
