0dba032bd02da407109b6596cae7885018d7ad32
Kishosre Kumar S <kishore061224@gmail.com>
2026-07-07 09:56:53 +0530

    First non repeat char uploaded

---
+++ src/Day_13/Home_task/Firstnonrepeatclass.java
@@ -0,0 +1,21 @@
+package Day_13.Home_task;
+import java.util.Arrays;
+public class Firstnonrepeatclass {
+    public static void main(String[] args){
+        String s = "swiss";
+        char[] arr =s.toCharArray();
+        //int count = 0;
+        for(int i=0;i<arr.length;i++){
+            int count = 0;
+            for(int j=0;j<arr.length;j++){
+                if(arr[i] == arr[j]){
+                    count++;
+                }
+            }
+            if(count == 1) {
+                System.out.print(arr[i]);
+                break;
+            }
+        }
+    }
+}
