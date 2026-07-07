f2da20d827692221a05ba2f21b7f67c865d09654
Kishosre Kumar S <kishore061224@gmail.com>
2026-07-07 10:10:19 +0530

    Student uploaded

--- src/Day_10/Student.java
+++ src/Day_10/Student.java
@@ -3,8 +3,8 @@ package Day_10;
 public class Student {
     public static void main(String[] args) {
         Ststic s1 = new Ststic();
-        s1.name = "keerthana";
-        s1.id = 123;
+        s1.name = "kishore kumar";
+        s1.id = 2224;
         System.out.println(s1.name);
         System.out.println(s1.id);
         s1.Study();
