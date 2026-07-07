609779a1ca3f2090e391df0ff3ef0ceed5091219
Kishosre Kumar S <kishore061224@gmail.com>
2026-07-07 10:10:04 +0530

    thread_runnable uploaded

---
+++ src/Day_14/Class_task/thread_runnable.java
@@ -0,0 +1,21 @@
+package Day_14.Class_task;
+
+public class thread_runnable {
+
+    public static void main(String [] args){
+        Mythread mt = new Mythread();
+        Thread t1 = new Thread(mt);
+        t1.start();
+        Thread t2 = new Thread(mt);
+        t2.start();
+        Thread t3 = new Thread(mt);
+        t3.start();
+//        t1.run();
+    }
+}
+class Mythread implements Runnable{
+    public void run(){
+        System.out.println(" second Thread running " + Thread.currentThread().getName());
+    }
+
+}
