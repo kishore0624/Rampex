e0e0f83a1fab7e800612faede152f8b2a329112f
Kishosre Kumar S <kishore061224@gmail.com>
2026-07-07 10:00:48 +0530

    Anagram uploaded

---
+++ src/Day_11/Anagram.java
@@ -0,0 +1,24 @@
+package Day_11;
+import java.util.Arrays;
+public class Anagram {
+    public static void main(String[] args) {
+        System.out.println(isAnagram("pot", "top"));
+
+    }
+
+    public static boolean isAnagram(String s1, String s2) {
+        if (s1.length() == s2.length()) {
+            char[] arr1 = s1.toCharArray();
+            char[] arr2 = s2.toCharArray();
+
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+
+            if (Arrays.equals(arr1,arr2) == true) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
