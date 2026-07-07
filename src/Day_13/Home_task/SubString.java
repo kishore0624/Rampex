48356e2a9e27ea1b24c3d47b001fbc06197bb8dd
Kishosre Kumar S <kishore061224@gmail.com>
2026-07-07 09:59:47 +0530

    SubString uploaded

---
+++ src/Day_13/Home_task/SubString.java
@@ -0,0 +1,13 @@
+package Day_13.Home_task;
+
+public class SubString {
+    public static void main(String[] args){
+        String a = "abc";
+        for(int i=0;i<a.length();i++){
+            for(int j=i+1;j<=a.length();j++){
+                System.out.println(a.substring(i,j));
+            }
+        }
+
+    }
+}
