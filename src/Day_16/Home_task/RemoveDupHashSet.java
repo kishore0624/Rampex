fad611b6f72ed8d07042669f31d12cae4b8e7ed5
Kishosre Kumar S <kishore061224@gmail.com>
2026-07-11 10:01:20 +0530

    RemoveDupHashSet uploaded

---
+++ src/Day_16/Home_task/RemoveDupHashSet.java
@@ -0,0 +1,18 @@
+package Day_16.Home_task;
+
+import java.util.HashSet;
+
+public class RemoveDupHashSet {
+    public static void main(String[] args){
+        HashSet<Integer> num = new HashSet<>();
+        num.add(1);
+        num.add(2);
+        num.add(2);
+        num.add(3);
+        num.add(4);
+        num.add(4);
+        num.add(5);
+        System.out.println(num);
+
+    }
+}
