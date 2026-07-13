7b700a5c28c0d89d52ab68223909755d7069020a
Kishosre Kumar S <kishore061224@gmail.com>
2026-07-13 09:29:33 +0530

    AvgGreaterthan50 uploaded

---
+++ src/Day_16/Class_task/AvgGreaterthan50.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class AvgGreaterthan50 {
+    public static void main(String[] args) {
+
+        int[] arr = {25, 60, 80, 40, 90, 55, 30, 100};
+
+        double avg = Arrays.stream(arr)
+                .filter(n -> n > 50)
+                .average()
+                .getAsDouble();
+
+        System.out.println(avg);
+    }
+}
