304eb964bb7847b544580aa4d20278a4430137b2
Kishosre Kumar S <kishore061224@gmail.com>
2026-07-15 11:19:46 +0530

    Stream uploaded

--- src/Day_20/Class_Task/Stream.java
+++ src/Day_20/Class_Task/Stream.java
@@ -1,6 +1,6 @@
 package Day_20.Class_Task;
 import java.util.*;
-public class Stream {
+public class Stream<S> {
     public static void main(String[] args) {
         int arr[] = {4,5,6,7,8,9,10,1,2,3};
         Arrays.stream(arr).filter((n)-> n%2!=0).sorted().forEach(n -> System.out.println(n));
