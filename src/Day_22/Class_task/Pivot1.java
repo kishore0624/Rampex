1d6331cecf34bd97c5bfdb8da699baa4a492ba61
Kishosre Kumar S <kishore061224@gmail.com>
2026-07-16 10:09:55 +0530

    Pivot uploaded

---
+++ src/Day_22/Class_task/Pivot1.java
@@ -0,0 +1,36 @@
+package Day_22.Class_task;
+import java.util.*;
+public class Pivot1 {
+    public static void main(String[] args) {
+        int[] arr = {10, 80, 30, 90, 40, 50, 70};
+        System.out.println(Arrays.toString(arr));
+
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+        static void quickSort(int [] arr,int low , int high) {
+        if(low < high){
+            int pi = partition(arr, low , high);
+            quickSort(arr, low, pi-1);
+            quickSort(arr, pi+1, high);
+        }
+
+    }
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+        return i+1;
+    }
+        }
+
