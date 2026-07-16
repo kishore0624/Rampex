718eb3aa6eec1d06bfd6aec135f3e92de15afc90
Kishosre Kumar S <kishore061224@gmail.com>
2026-07-16 10:20:34 +0530

    HashDemo uploaded

---
+++ src/Day_17/Class_task/hashmapdemo.java
@@ -0,0 +1,15 @@
+package Day_17.Class_task;
+
+import java.util.HashMap;
+
+public class hashmapdemo {
+    static void main(String[] args) {
+        HashMap<Integer, String> s = new HashMap<>();
+        s.put(1, "one");
+        s.put(2, "two");
+        s.put(3, "three");
+        System.out.println(s);
+
+    }
+
+}
